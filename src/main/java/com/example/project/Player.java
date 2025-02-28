033aa515dba1671dc458c84a09d5ca9ae145b53f
WenxinLi0317 <wenxinl8@nycstudents.net>
2025-02-28 13:26:02 +0000

    Fix conflicts

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -28,15 +28,17 @@ public class Player{
 
     public ArrayList<Integer> findRankingFrequency(){
         ArrayList<Integer>freq = new ArrayList<>();
+        //4449j
         //"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-        //
+        //[ 0 ,  0 ,  3 ,  0 ,  0 ,  0 ,  0 ,  1 ,   0 ,  1 ,  0 ,  0  , 0 ]
+
         for(int j = 0; j < 13; j++)
         {
             for(int i = 0; i < ranks.length; i++)
             {
                 if(allCards.contains(ranks[i]))
                 {
-    
+                    //freq.set(j, null)
                 }
             }
         }
